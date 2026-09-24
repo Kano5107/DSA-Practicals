2266533c295ee35c986ce6119e48831d6cd836ad
25DCS008 <25dcs008@charusat.edu.in>
2026-09-24 15:05:30 +0530

    Update pra6_p3.cpp

--- pra6_p3.cpp
+++ pra6_p3.cpp
@@ -1,54 +1,45 @@
+
 #include <iostream>
 #include <stack>
+#include <string>
+#include <cctype>
 using namespace std;
 
-int precedence(char ch)
-{
-    if(ch == '^')
+int precedence(char ch) {
+    if (ch == '^')
         return 3;
-
-    if(ch == '*' || ch == '/' || ch == '%')
+    if (ch == '*' || ch == '/' || ch == '%')
         return 2;
-
-    if(ch == '+' || ch == '-')
+    if (ch == '+' || ch == '-')
         return 1;
-
     return 0;
 }
 
-string infixToPostfix(string s)
-{
+string infixToPostfix(string s) {
     stack<char> st;
     string ans = "";
 
-    for(char ch : s)
-    {
-        if((ch >= 'a' && ch <= 'z') ||
-           (ch >= 'A' && ch <= 'Z') ||
-           (ch >= '0' && ch <= '9'))
-        {
+    for (char ch : s) {
+        if (isalnum(static_cast<unsigned char>(ch))) {
             ans += ch;
         }
-        else if(ch == '(')
-        {
+        else if (ch == '(') {
             st.push(ch);
         }
-        else if(ch == ')')
-        {
-            while(!st.empty() && st.top() != '(')
-            {
+        else if (ch == ')') {
+            while (!st.empty() && st.top() != '(') {
                 ans += st.top();
                 st.pop();
             }
 
-            if(!st.empty())
+            if (!st.empty())
                 st.pop();
         }
-        else
-        {
-            while(!st.empty() &&
-                  precedence(st.top()) >= precedence(ch))
-            {
+        else {
+            while (!st.empty() && st.top() != '(' &&
+                  (precedence(st.top()) > precedence(ch) ||
+                  (precedence(st.top()) == precedence(ch)
+                   && ch != '^'))) {
                 ans += st.top();
                 st.pop();
             }
@@ -57,8 +48,7 @@ string infixToPostfix(string s)
         }
     }
 
-    while(!st.empty())
-    {
+    while (!st.empty()) {
         ans += st.top();
         st.pop();
     }
@@ -66,12 +56,11 @@ string infixToPostfix(string s)
     return ans;
 }
 
-int main()
-{
+int main() {
     string s;
     cin >> s;
 
-    cout << infixToPostfix(s);
+    cout << infixToPostfix(s) << endl;
 
     return 0;
 }
